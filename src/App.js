71c7d9b5a1db8feec6e3f971cafafa8470f915c5
SeikhAbubakar5 <seikhabubakar47@gmail.com>
2024-12-13 16:42:03 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    };
+  }
+
+  render() {
+    return (
+
+      <div className='App'>
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <div>
+          <button onClick={() => this.setState({ count: this.state.count + 1 })}>Increment</button>
+          <button onClick={() => this.setState({ count: this.state.count - 1 })}>Increment</button>
+        </div>
+      </div>
+    )
+  }
+}
+export default App
+
+
